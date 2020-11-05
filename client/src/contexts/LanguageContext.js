3bac9bfba6cd7cf13e07aabb4e2c38e56da7a20b
Afuza <theafuza@gmail.com>
2020-11-05 19:48:05 +0700

    set default language to english

--- client/src/contexts/LanguageContext.js
+++ client/src/contexts/LanguageContext.js
@@ -3,7 +3,7 @@ import React, { useState, createContext } from "react";
 export const LanguageContext = createContext();
 
 export const LanguageProvider = (props) => {
-  const [language, setLanguage] = useState("indonesian");
+  const [language, setLanguage] = useState("english");
 
   return (
     <LanguageContext.Provider value={[language, setLanguage]}>
